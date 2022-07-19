e37d23c3e1a82dbd3616eaffb958e489b2c77587
Robert Bornschein <robertbornscheinofficial@gmail.com>
2022-07-19 10:01:14 +0200

    chore: copy over en-US -> de-DE

--- src/components/modal/add-rule.modal.tsx
+++ src/components/modal/add-rule.modal.tsx
@@ -109,7 +109,7 @@ export const AddRuleModal = forwardRef<HTMLDivElement, IAddRuleModalProps>(
                       ref={inputRef}
                       value={rule.title}
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                      placeholder={placeholder || "No Rule Set"}
+                      placeholder={placeholder || t("no-rule-set")}
                       onChange={onChange}
                       required
                     />
