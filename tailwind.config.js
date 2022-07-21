ec0e259b6c687772956de40021309ca85e37e5a9
Robert Bornschein <robertbornscheinofficial@gmail.com>
2022-07-21 03:19:38 +0200

    feat: wiggle-animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '20%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-8deg)' },
+          '65%': { transform: 'rotate(14deg)' },
+          '800%': { transform: 'rotate(-4deg)' },
+          '100%': {transform: 'rotate(0deg)'}
+        },
+      },
+      animation:{
+        'wiggle': 'wiggle 2s linear infinite'
+      }
+    },
   },
   plugins: [],
 }
